0c3946578d1c1c84e15fa455e6e3c0de4b09ee0e
XiaoDi Ma <i@mxd.moe>
2017-11-17 09:59:18 +0800

    menu ready

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -8,16 +8,62 @@ export const MENU_ITEMS: NbMenuItem[] = [
     home: true,
   },
   {
-    title: '功能管理',
+    title: '常规设置',
     group: true,
   },
   {
-    title: 'nav',
-    icon: 'nb-locked',
+    title: '账户管理',
+    icon: 'ion-person',
     children: [
       {
-        title: '登陆',
+        title: '正版验证',
         link: '/auth/login',
+      },
+    ],
+  },
+  {
+    title: '角色管理',
+    icon: 'ion-person-stalker',
+    children: [
+      {
+        title: '角色列表',
+        link: '/auth/login',
+      },
+    ],
+  },
+  {
+    title: '高级设置',
+    group: true,
+  },
+  {
+    title: '服务器管理',
+    icon: 'ion-grid',
+    children: [
+      {
+        title: '服务器列表',
+        link: '/auth/login',
+      },
+      {
+        title: '玩家管理',
+        link: '/ergergerg',
+      },
+    ],
+  },
+  {
+    title: 'RMCA管理',
+    icon: 'ion-cube',
+    children: [
+      {
+        title: '管理员列表',
+        link: '/auth/login',
+      },
+      {
+        title: '服务器历史管理',
+        link: '/auth/login',
+      },
+      {
+        title: '财务历史管理',
+        link: '/asdasdasr',
       },
     ],
   },
