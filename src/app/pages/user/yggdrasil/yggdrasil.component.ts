368a39b876983baa206e9b82e1c906607c52612f
XiaoDi Ma <i@mxd.moe>
2017-11-24 10:12:55 +0800

    模板搞定, 等接口

--- src/app/pages/user/yggdrasil/yggdrasil.component.ts
+++ src/app/pages/user/yggdrasil/yggdrasil.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ngx-profile',
   styleUrls: ['./yggdrasil.component.scss'],
   templateUrl: './yggdrasil.component.html',
 })
-export class YggdrasilComponent {
+export class YggdrasilComponent implements OnInit {
+
+  yggdrasil: any = {};
+  submitted: boolean;
+
+  ngOnInit() {
+    this.yggdrasil = {
+      username: '',
+      password: '',
+    };
+  }
+
+  isSubmitted(): boolean {
+    return this.submitted;
+  }
 }
