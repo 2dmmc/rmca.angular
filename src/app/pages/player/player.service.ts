daed2d5ea1a316f07cf50ca9bf6c9efbabc6625d
XiaoDi Ma <i@mxd.moe>
2017-11-30 14:48:04 +0800

    service的文档先写起来

--- src/app/pages/player/player.service.ts
+++ src/app/pages/player/player.service.ts
@@ -7,12 +7,34 @@ export class PlayerService {
   constructor(private http: HttpClient) {
   }
 
-  getRoles() {
+  /**
+   * @name 获取角色列表
+   * @description 获取当前用户的角色列表, 返回Role的集合
+   * @return {Promise<Object>}
+   */
+  getRoles(): Promise<object> {
     return this.http.get('/api/role')
       .toPromise();
   }
 
-  addRole(rolename) {
+  /**
+   * @name 获取角色详情
+   * @description 获取某个角色的详情, 入参为待获取详情的角色ID, 返回不完整Role模型
+   * @param {string} roleId 角色ID
+   * @return {Promise<Object>}
+   */
+  getRole(roleId: string): Promise<object> {
+    return this.http.get(`/api/role/${roleId}`)
+      .toPromise();
+  }
+
+  /**
+   * @name 新增角色
+   * @description 新增角色, 入参为角色名称, 返回新增结果
+   * @param {string} rolename 角色名称
+   * @return {Promise<Object>}
+   */
+  addRole(rolename: string): Promise<object> {
     const params = new HttpParams()
       .set('rolename', rolename);
 
@@ -20,17 +42,15 @@ export class PlayerService {
       .toPromise();
   }
 
-  getRoleSkin(roleId) {
-    return this.http.get(`/api/role/skin/${roleId}`)
-      .toPromise();
-  }
-
-  getRole(roleId) {
-    return this.http.get(`/api/role/${roleId}`)
-      .toPromise();
-  }
-
-  updateRole(roleId, skinModel, skin) {
+  /**
+   * @name 更新角色详情
+   * @description 更新某个角色的详情信息, 入参为要更新的字段, 返回更新结果
+   * @param {string} roleId 角色id
+   * @param {string} skinModel 皮肤模型
+   * @param {string} skin 皮肤
+   * @return {Promise<Object>}
+   */
+  updateRole(roleId: string, skinModel: string, skin: string): Promise<object> {
     // FIXME https://github.com/angular/angular/issues/18261
     // FUCK u Angular
     // const params = new HttpParams()
@@ -44,12 +64,24 @@ export class PlayerService {
       .toPromise();
   }
 
-  updateYggdrasilSkin(roldId) {
+  /**
+   * @name 同步正版验证皮肤
+   * @description 从Mojang服务器同步皮肤至某个角色, 入参为角色ID, 返回同步结果
+   * @param {string} roldId 角色ID
+   * @return {Promise<Object>}
+   */
+  updateYggdrasilSkin(roldId: string): Promise<object> {
     return this.http.patch(`/api/role/skin/${roldId}/yggdrasil`, {})
       .toPromise();
   }
 
-  updateDefaultRole(roleId) {
+  /**
+   * @name 更新默认角色
+   * @description 更新某个角色为默认角色, 入参为角色ID, 返回更新结果
+   * @param {string} roleId 角色ID
+   * @return {Promise<Object>}
+   */
+  updateDefaultRole(roleId: string): Promise<object> {
     const params = new HttpParams()
       .set('roleId', roleId);
 
--- src/app/pages/user/user.service.ts
+++ src/app/pages/user/user.service.ts
@@ -7,12 +7,23 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
 
-  getUserProfile() {
+  /**
+   * @name 获取用户个人资料
+   * @description 获取当前用户的个人资料, 返回不完整的User模型
+   * @return {Promise<Object>}
+   */
+  getUserProfile(): Promise<object> {
     return this.http.get('/api/user/profile')
       .toPromise();
   }
 
-  updateUserProfile(email) {
+  /**
+   * @name 更新用户个人资料
+   * @description 更新当前用户的个人资料, 入参为要更新的字段, 返回更新结果
+   * @param {string} email 电子邮箱
+   * @return {Promise<Object>}
+   */
+  updateUserProfile(email: string): Promise<object> {
     const params = new HttpParams()
       .set('email', email);
 
@@ -20,7 +31,14 @@ export class UserService {
       .toPromise();
   }
 
-  updateUserPassword(password, newPassword) {
+  /**
+   * @name 更新用户密码
+   * @description 更新当前用户的密码, 入参为当前用户的当前密码和新密码, 返回更新结果
+   * @param {string} password 当前密码
+   * @param {string} newPassword 新密码
+   * @return {Promise<Object>}
+   */
+  updateUserPassword(password: string, newPassword: string): Promise<object> {
     const params = new HttpParams()
       .set('password', password)
       .set('newPassword', newPassword);
@@ -29,7 +47,14 @@ export class UserService {
       .toPromise();
   }
 
-  updateUserYggdrasil(username, password) {
+  /**
+   * @name 更新正版验证资料
+   * @description 更新当前用户的Minecraft正版验证资料, 入参为Mojang用户名和密码, 返回更新结果
+   * @param {string} username Mojang用户名
+   * @param {string} password Mojang密码
+   * @return {Promise<Object>}
+   */
+  updateUserYggdrasil(username: string, password: string): Promise<object> {
     const params = new HttpParams()
       .set('username', username)
       .set('password', password);
@@ -38,12 +63,23 @@ export class UserService {
       .toPromise();
   }
 
-  verifyEmail(hash) {
+  /**
+   * @name 验证电子邮箱
+   * @description 验证当前用户电子邮箱是否有效, 入参为附在URL中的Hash, 返回验证结果
+   * @param {string} hash URL中的hash
+   * @return {Promise<Object>}
+   */
+  verifyEmail(hash: string): Promise<object> {
     return this.http.patch(`/api/user/email-verify/${hash}`, {})
       .toPromise();
   }
 
-  resendVerifyEmail() {
+  /**
+   * @name 重新发送验证邮件
+   * @description 重新发送"验证用户电子邮箱是否有效"的电子邮件, 返回发送结果
+   * @return {Promise<Object>}
+   */
+  resendVerifyEmail(): Promise<object> {
     return this.http.get('/api/user/resend-verify-email')
       .toPromise();
   }
