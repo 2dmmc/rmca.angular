0b080b74d9014d50cb7c05838d2f2e2f63793059
XiaoDi Ma <i@mxd.moe>
2017-12-01 15:13:33 +0800

    社交模块完成一个

--- src/app/pages/user/socials/qq-state/social-QQ-state.component.ts
+++ src/app/pages/user/socials/qq-state/social-QQ-state.component.ts
@@ -1,15 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {UserModel} from '../../user.model';
 
 @Component({
   selector: 'ngx-social-qq-state',
   styleUrls: ['./social-QQ-state.component.scss'],
   templateUrl: './social-QQ-state.component.html',
 })
-export class SocialQQStateComponent {
+export class SocialQQStateComponent implements OnInit {
   constructor() {
   }
 
-  @Input() user: any;
+  @Input() user: UserModel;
+
+  ngOnInit(): void {
+    // if (this.qq) {
+    //   this.qq.isBind = true;
+    // } else {
+    //   this.qq = {
+    //     avatar: '/images/user/sdjnmxd.jpg',
+    //     name: '未绑定',
+    //     isBind: false,
+    //   };
+    // }
+    console.info(this.user);
+  }
 
   oAuth(): void {
     window.location.href = `https://auth.bangbang93.com/qq/oauth?callbackUrl=${window.location.origin}/callback/oauth/qq`;
--- src/app/pages/user/socials/socials.component.ts
+++ src/app/pages/user/socials/socials.component.ts
@@ -1,31 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {UserService} from '../user.service';
 import {NoticeService} from '../../../@system/notice/notice.service';
+
+import {UserModel} from '../user.model';
 
 @Component({
   selector: 'ngx-socials',
   styleUrls: ['./socials.component.scss'],
   templateUrl: './socials.component.html',
 })
-export class SocialsComponent implements OnInit {
+export class SocialsComponent implements AfterViewInit {
   constructor(private userService: UserService,
               private noticeService: NoticeService) {
+
   }
 
-  user: any;
+  user: UserModel;
 
-  ngOnInit() {
-    this.user = {
-      email: '',
-    };
-
+  ngAfterViewInit(): void {
     this.getUserProfile();
   }
 
   getUserProfile(): void {
     this.userService.getUserProfile()
       .then(userProfile => {
-        this.user = userProfile;
+        this.user = userProfile as UserModel;
       })
       .catch(error => {
         this.noticeService.error('获取用户信息失败, 请刷新页面重试', `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`);
