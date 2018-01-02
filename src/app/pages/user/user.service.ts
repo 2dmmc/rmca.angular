111c38d1d3b118a138681abe776ba1ab4b4dd5a3
XiaoDi Ma <i@mxd.moe>
2018-01-02 16:13:16 +0800

    fix issueˆ

--- src/app/pages/user/user.service.ts
+++ src/app/pages/user/user.service.ts
@@ -23,6 +23,7 @@ export class UserService {
    * @param {string} email 电子邮箱
    * @return {Promise<Object>}
    */
+  public async updateUserProfile(email: string): Promise<object> {
     const params = new HttpParams()
       .set('email', email);
 
