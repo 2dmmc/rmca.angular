a3f9056f505a0d264a56fc3837a943a67b614243
XiaoDi Ma <i@mxd.moe>
2018-08-23 15:29:57 +0800

    callback模块重构完了

--- src/app/callback/components/login-qq/login-QQ.component.ts
+++ src/app/callback/components/login-qq/login-QQ.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
 import {CallbackService} from '../../../@core/data/callback.service';
+import {CommonUtilService} from '../../../@core/utils/common-util.service';
+
+import {CallbackNoticeComponent} from '../callback-notice/callback-notice.component';
 
 @Component({
   selector: 'ngx-login-qq',
@@ -9,42 +12,39 @@ import {CallbackService} from '../../../@core/data/callback.service';
 })
 
 export class LoginQQComponent implements OnInit {
-  notice: {
-    type: 'info' | 'success' | 'danger',
-    title: string,
-    message: string,
-  };
+  @ViewChild(CallbackNoticeComponent) notice: CallbackNoticeComponent;
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
-              private callbackService: CallbackService) {
-    this.sendNotice('info', '请稍后...', '登录中...');
+              private callbackService: CallbackService,
+              private commonUtilService: CommonUtilService) {
   }
 
   public ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(queryParams => {
-      this.callbackService.loginQQCallback(queryParams.accessToken, queryParams.expiresIn)
-        .then(success => {
-          this.sendNotice('success', '授权登陆成功', '即将跳转到dashboard');
-          setTimeout(() => {
-            this.router.navigate(['/pages/dashboard']);
-          }, 3e3);
-        })
-        .catch(error => {
-          this.sendNotice(
-            'danger',
-            '授权登陆失败',
-            `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
-          );
-        });
-    });
-  }
+    this.notice.show(
+      'info',
+      '请稍候...',
+      '登录中...',
+    );
 
-  private sendNotice(type: 'info' | 'success' | 'danger', title: string, message: string): void {
-    this.notice = {
-      type: type,
-      title: title,
-      message: message,
-    };
+    this.activatedRoute.queryParams.subscribe(async (queryParams) => {
+      try {
+        await this.callbackService.loginQQCallback(queryParams.accessToken, queryParams.expiresIn);
+        this.notice.show(
+          'success',
+          '授权登陆成功',
+          '即将跳转到dashboard',
+        );
+        await this.commonUtilService.sleep(3e3);
+        this.router.navigate(['/pages/dashboard']);
+      } catch (error) {
+        this.notice.show(
+          'danger',
+          '授权登陆失败',
+          `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
+        );
+        console.error(error);
+      }
+    });
   }
 }
--- src/app/callback/components/oauth-weibo/oauth-weibo.component.ts
+++ src/app/callback/components/oauth-weibo/oauth-weibo.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
 import {CallbackService} from '../../../@core/data/callback.service';
+import {CommonUtilService} from '../../../@core/utils/common-util.service';
+
+import {CallbackNoticeComponent} from '../callback-notice/callback-notice.component';
 
 @Component({
   selector: 'ngx-oauth-weibo',
@@ -9,38 +12,39 @@ import {CallbackService} from '../../../@core/data/callback.service';
 })
 
 export class OauthWeiboComponent implements OnInit {
-  notice: {
-    type: 'info' | 'success' | 'danger',
-    title: string,
-    message: string,
-  };
+  @ViewChild(CallbackNoticeComponent) notice: CallbackNoticeComponent;
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
-              private callbackService: CallbackService) {
-    this.sendNotice('info', '请稍后...', '授权中...');
+              private callbackService: CallbackService,
+              private commonUtilService: CommonUtilService) {
   }
 
   public ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(queryParams => {
-      this.callbackService.oauthWeiboCallback(queryParams.accessToken, queryParams.expiresIn)
-        .then(success => {
-          this.sendNotice('success', '授权成功', '即将跳转到dashboard');
-          setTimeout(() => {
-            this.router.navigate(['/pages/user/socials']);
-          }, 3e3);
-        })
-        .catch(error => {
-          this.sendNotice('danger', '授权失败', `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`);
-        });
-    });
-  }
+    this.notice.show(
+      'info',
+      '请稍候...',
+      '授权中...',
+    );
 
-  private sendNotice(type: 'info' | 'success' | 'danger', title: string, message: string): void {
-    this.notice = {
-      type: type,
-      title: title,
-      message: message,
-    };
+    this.activatedRoute.queryParams.subscribe(async (queryParams) => {
+      try {
+        await this.callbackService.oauthWeiboCallback(queryParams.accessToken, queryParams.expiresIn);
+        this.notice.show(
+          'success',
+          '授权成功',
+          '即将跳转到dashboard',
+        );
+        await this.commonUtilService.sleep(3e3);
+        this.router.navigate(['/pages/user/socials']);
+      } catch (error) {
+        this.notice.show(
+          'danger',
+          '授权失败',
+          `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
+        );
+        console.error(error);
+      }
+    });
   }
 }
