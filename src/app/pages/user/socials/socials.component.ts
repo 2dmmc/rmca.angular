d78155c55cc822f7f4cd6101e6bb855cb6de7e85
XiaoDi Ma <i@mxd.moe>
2018-08-24 16:29:54 +0800

    user功能展示ready

--- src/app/pages/user/socials/socials.component.ts
+++ src/app/pages/user/socials/socials.component.ts
@@ -2,10 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 import {NoticeService} from '../../../@core/services/notice.service';
 
-import {UserService} from '../../../@core/data/user.service';
-
 import {IUser} from '../../../@model/common/user/user.interface';
-import {DefaultUser} from '../../../@model/common/user/user.const';
+import {AuthUtilService} from '../../../@core/utils/auth-util.service';
 
 @Component({
   styleUrls: ['./socials.component.scss'],
@@ -13,28 +11,13 @@ import {DefaultUser} from '../../../@model/common/user/user.const';
 })
 
 export class SocialsComponent implements OnInit {
-  user: IUser;
+  public user: IUser;
 
   constructor(private noticeService: NoticeService,
-              private userService: UserService) {
-    this.user = DefaultUser;
+              private authUtilService: AuthUtilService) {
   }
 
   public ngOnInit(): void {
-    // this.user = this.userCacheService.getCache();
-  }
-
-  public async getUserProfile(): Promise<void> {
-    try {
-      const user = await this.userService.getUserProfile();
-
-      // this.userCacheService.setCache(user as IUser);
-      this.user = user as IUser;
-    } catch (error) {
-      this.noticeService.error(
-        '获取用户信息失败, 请刷新页面重试',
-        `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
-      );
-    }
+    this.user = this.authUtilService.user;
   }
 }
--- src/app/pages/user/yggdrasil/yggdrasil.component.ts
+++ src/app/pages/user/yggdrasil/yggdrasil.component.ts
@@ -2,33 +2,27 @@ import {Component, OnInit} from '@angular/core';
 
 import {NoticeService} from '../../../@core/services/notice.service';
 
-import {UserService} from '../../../@core/data/user.service';
-
 import {IUser} from '../../../@model/common/user/user.interface';
-import {DefaultUser} from '../../../@model/common/user/user.const';
+import {AuthUtilService} from '../../../@core/utils/auth-util.service';
 
 @Component({
   styleUrls: ['./yggdrasil.component.scss'],
   templateUrl: './yggdrasil.component.html',
 })
 export class YggdrasilComponent implements OnInit {
-  user: IUser;
+  public user: IUser;
 
   constructor(private noticeService: NoticeService,
-              private userService: UserService) {
-    this.user = DefaultUser;
+              private authUtilService: AuthUtilService) {
   }
 
   ngOnInit() {
-    // this.user = this.userCacheService.getCache();
+    this.user = this.authUtilService.user;
   }
 
   public async getUserProfile(): Promise<void> {
     try {
-      const user = await this.userService.getUserProfile();
-
-      // this.userCacheService.setCache(user as IUser);
-      this.user = user as IUser;
+      this.user = await this.authUtilService.updateUser();
     } catch (error) {
       this.noticeService.error(
         '获取用户信息失败, 请刷新页面重试',
