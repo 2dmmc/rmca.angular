7fb0526e766b5bb46cc25485e6b65df09ac58cf6
XiaoDi Ma <i@mxd.moe>
2018-08-27 10:56:37 +0800

    profile/user-password重构完成

--- src/app/pages/user/profile/user-password/profile-user-password.component.ts
+++ src/app/pages/user/profile/user-password/profile-user-password.component.ts
@@ -1,7 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 import {UserService} from '../../../../@core/data/user.service';
 import {NoticeService} from '../../../../@core/services/notice.service';
+import {CommonUtilService} from '../../../../@core/utils/common-util.service';
+
+import {passwordEqualValidator} from '../../../../@core/directives';
 
 @Component({
   selector: 'ngx-profile-user-password',
@@ -9,12 +13,39 @@ import {NoticeService} from '../../../../@core/services/notice.service';
   templateUrl: './profile-user-password.component.html',
 })
 
-export class ProfileUserPasswordComponent {
-  submitted: boolean;
+export class ProfileUserPasswordComponent implements OnInit {
+  public changePasswordForm: FormGroup;
+  public submitted: boolean;
 
   constructor(private noticeService: NoticeService,
-              private userService: UserService) {
+              private userService: UserService,
+              private commonUtilService: CommonUtilService) {
     this.submitted = false;
+  }
+
+  public ngOnInit() {
+    this.changePasswordForm = new FormGroup({
+      nowPassword: new FormControl(
+        '', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(32),
+        ],
+      ),
+      password: new FormControl(
+        '', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(32),
+        ],
+      ),
+      repeatPassword: new FormControl(
+        '', [
+          Validators.required,
+          passwordEqualValidator,
+        ],
+      ),
+    });
   }
 
   public async updatePassword(passwordForm) {
@@ -22,11 +53,8 @@ export class ProfileUserPasswordComponent {
 
     try {
       await this.userService.updateUserPassword(passwordForm.password, passwordForm.newPassword);
-      this.submitted = false;
       this.noticeService.success('更新密码成功', '更新密码成功');
     } catch (error) {
-      this.submitted = false;
-
       let errorMessage = '';
 
       switch (error.status) {
@@ -38,8 +66,11 @@ export class ProfileUserPasswordComponent {
           errorMessage = `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`;
         }
       }
-
       this.noticeService.error('更新密码失败', errorMessage);
+      console.error(error);
     }
+
+    await this.commonUtilService.sleep(0.7e3);
+    this.submitted = false;
   }
 }
