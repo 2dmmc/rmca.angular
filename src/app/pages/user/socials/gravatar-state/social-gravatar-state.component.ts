55d63b192dd3af4a2e1ba0eb57c7f38fbbc981ff
XiaoDi Ma <i@mxd.moe>
2018-08-30 16:50:21 +0800

    重构格式

--- src/app/pages/user/socials/gravatar-state/social-gravatar-state.component.ts
+++ src/app/pages/user/socials/gravatar-state/social-gravatar-state.component.ts
@@ -5,6 +5,7 @@ import {NoticeService} from '../../../../@core/services/notice.service';
 import {IUser} from '../../../../@model/common/user/user.interface';
 import {UserService} from '../../../../@core/data/user.service';
 import {AuthUtilService} from '../../../../@core/utils/auth-util.service';
+import {CommonUtilService} from '../../../../@core/utils/common-util.service';
 
 @Component({
   selector: 'ngx-social-gravatar-state',
@@ -13,13 +14,18 @@ import {AuthUtilService} from '../../../../@core/utils/auth-util.service';
 })
 export class SocialGravatarStateComponent {
   @Input() user: IUser;
+  public updating: boolean;
 
   constructor(private userService: UserService,
               private noticeService: NoticeService,
-              private authUtilService: AuthUtilService) {
+              private authUtilService: AuthUtilService,
+              private commonUtilService: CommonUtilService) {
+    this.updating = false;
   }
 
   public async updateUserAvatar() {
+    this.updating = true;
+
     try {
       await this.userService.updateUserAvatar('gravatar');
       this.noticeService.success(
@@ -34,5 +40,8 @@ export class SocialGravatarStateComponent {
       const errorMessage = errorMessageMap[error.status] || '未知错误, 请联系鹳狸猿';
       this.noticeService.error('更换头像失败', errorMessage);
     }
+
+    await this.commonUtilService.sleep(0.7e3);
+    this.updating = false;
   }
 }
--- src/app/pages/user/socials/qq-state/social-QQ-state.component.ts
+++ src/app/pages/user/socials/qq-state/social-QQ-state.component.ts
@@ -5,6 +5,7 @@ import {NoticeService} from '../../../../@core/services/notice.service';
 import {IUser} from '../../../../@model/common/user/user.interface';
 import {UserService} from '../../../../@core/data/user.service';
 import {AuthUtilService} from '../../../../@core/utils/auth-util.service';
+import {CommonUtilService} from '../../../../@core/utils/common-util.service';
 
 @Component({
   selector: 'ngx-social-qq-state',
@@ -13,18 +14,24 @@ import {AuthUtilService} from '../../../../@core/utils/auth-util.service';
 })
 export class SocialQQStateComponent {
   @Input() user: IUser;
+  public updating: boolean;
 
   constructor(private userService: UserService,
               private noticeService: NoticeService,
-              private authUtilService: AuthUtilService) {
+              private authUtilService: AuthUtilService,
+              private commonUtilService: CommonUtilService) {
+    this.updating = false;
   }
 
   public oAuth(): void {
+    this.updating = true;
     window.location.href =
       `https://auth.bangbang93.com/qq/oauth?callbackUrl=${window.location.origin}/callback/oauth/qq`;
   }
 
   public async updateUserAvatar() {
+    this.updating = true;
+
     try {
       await this.userService.updateUserAvatar('qq');
       this.noticeService.success(
@@ -39,5 +46,8 @@ export class SocialQQStateComponent {
       const errorMessage = errorMessageMap[error.status] || '未知错误, 请联系鹳狸猿';
       this.noticeService.error('更换头像失败', errorMessage);
     }
+
+    await this.commonUtilService.sleep(0.7e3);
+    this.updating = false;
   }
 }
