7c52b0f5a72343cb028c47cb9f915149324e1c17
XiaoDi Ma <i@mxd.moe>
2018-11-09 17:03:51 +0800

    重构半成品

--- src/app/pages/admin/rmca/users/users.component.ts
+++ src/app/pages/admin/rmca/users/users.component.ts
@@ -15,90 +15,66 @@ import {UserUnbanModalComponent} from './user-unban-modal/user-unban-modal.compo
 })
 
 export class UsersComponent implements OnInit {
-  filter: {
-    type: string,
-    types: any,
-  };
-  search: {
-    keyWord: string,
-    submitted: boolean;
-  };
-  users: IAdminRmcaUsersUser[];
-  page: number;
-  limit: number;
+  public users: IAdminRmcaUsersUser[];
+  public page: number;
+  public limit: number;
+  public count: number;
+
+
+  public loading: boolean;
 
   constructor(private noticeService: NoticeService,
               private modalService: NgbModal,
               private rmcaService: RmcaService,
               private router: Router) {
-    this.search = {
-      keyWord: '',
-      submitted: false,
-    };
-    this.filter = {
-      type: 'all',
-      types: {
-        all: 'all',
-        onlyAdmin: 'onlyAdmin',
-        onlyBan: 'onlyBan',
-      },
-    };
     this.users = [];
     this.page = 1;
     this.limit = 10;
+    this.count = 1;
+    this.loading = false;
   }
 
   public ngOnInit(): void {
     this.getUsers(this.page, this.limit);
   }
 
-  public filterAll(): void {
-    this.page = 1;
-    this.getUsers(this.page, this.limit);
-  }
+  // public filterAll(): void {
+  //   this.page = 1;
+  //   this.getUsers(this.page, this.limit);
+  // }
+  //
+  // public filterAdmin(): void {
+  //   this.page = 1;
+  //   this.searchUser(this.search.keyWord, this.page, this.limit, true, false);
+  // }
+  //
+  // public filterBanned(): void {
+  //   this.page = 1;
+  //   this.searchUser(this.search.keyWord, this.page, this.limit, false, true);
+  // }
+  //
+  // public searchClick(): void {
+  //   this.page = 1;
+  //   this.searchUser(this.search.keyWord, this.page, this.limit,
+  //     this.filter.type === this.filter.types.onlyAdmin,
+  //     this.filter.type === this.filter.types.onlyBan);
+  // }
 
-  public filterAdmin(): void {
-    this.page = 1;
-    this.searchUser(this.search.keyWord, this.page, this.limit, true, false);
-  }
+  public async getUsers(page: number, limit: number): Promise<void> {
+    this.loading = true;
 
-  public filterBanned(): void {
-    this.page = 1;
-    this.searchUser(this.search.keyWord, this.page, this.limit, false, true);
-  }
-
-  public searchClick(): void {
-    this.page = 1;
-    this.searchUser(this.search.keyWord, this.page, this.limit,
-      this.filter.type === this.filter.types.onlyAdmin,
-      this.filter.type === this.filter.types.onlyBan);
-  }
-
-  public searchKeyDown(event): void {
-    if (event.keyCode === 13) {
-      this.searchClick();
+    try {
+      const users = await this.rmcaService.getUsers(page, limit);
+      this.users = users['data'];
+      this.count = users['count'];
+    } catch (error) {
+      this.noticeService.error('获取用户列表失败', '获取用户列表失败, 请刷新页面重试');
     }
+
+    this.loading = false;
   }
 
-  public pageAdd(): void {
-    this.page++;
-    this._pageChange(this.page);
-  }
-
-  public pageKeyDown(event): void {
-    if (event.keyCode === 13) {
-      this._pageChange(this.page);
-    }
-  }
-
-  public pageMinus(): void {
-    if (this.page > 1) {
-      this.page--;
-      this._pageChange(this.page);
-    }
-  }
-
-  public banUser(user: IAdminRmcaUsersUser): void {
+  public openBanUserModal(user: IAdminRmcaUsersUser): void {
     const activeModal = this.modalService.open(UserBanModalComponent, {
       size: 'lg',
       container: 'nb-layout',
@@ -111,7 +87,7 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  public unBanUser(user: IAdminRmcaUsersUser): void {
+  public openUnBanUserModal(user: IAdminRmcaUsersUser): void {
     const activeModal = this.modalService.open(UserUnbanModalComponent, {
       size: 'lg',
       container: 'nb-layout',
@@ -124,42 +100,24 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  private getUsers(page: number, limit: number): void {
-    this.rmcaService.getUsers(page, limit)
-      .then(users => {
-        this.users = users as IAdminRmcaUsersUser[];
-      })
-      .catch(error => {
-        this.noticeService.error(
-          '获取用户列表失败, 请刷新页面重试',
-          `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
-        );
-      });
-  }
-
   public goToUserDetail(userId): void {
     this.router.navigate(['/pages/admin/rmca/user', userId]);
   }
 
-  private searchUser(keyword: string, page: number, limit: number, isAdmin?: boolean, isBanned?: boolean): void {
-    this.search.submitted = true;
-
-    this.rmcaService.searchUsers(keyword, page, limit, isAdmin, isBanned)
-      .then(users => {
-        this.search.submitted = false;
-        this.users = users as IAdminRmcaUsersUser[];
-      })
-      .catch(error => {
-        this.search.submitted = false;
-        this.noticeService.error(
-          '获取搜索结果失败',
-          `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
-        );
-      });
-  }
-
-  private _pageChange(page): void {
-    this.filter.type = this.filter.types.all;
-    this.getUsers(page, this.limit);
-  }
+  // private searchUser(keyword: string, page: number, limit: number, isAdmin?: boolean, isBanned?: boolean): void {
+  //   this.search.submitted = true;
+  //
+  //   this.rmcaService.searchUsers(keyword, page, limit, isAdmin, isBanned)
+  //     .then(users => {
+  //       this.search.submitted = false;
+  //       this.users = users as IAdminRmcaUsersUser[];
+  //     })
+  //     .catch(error => {
+  //       this.search.submitted = false;
+  //       this.noticeService.error(
+  //         '获取搜索结果失败',
+  //         `message: ${error.error.message || '未知'} | code: ${error.status || '未知'}`,
+  //       );
+  //     });
+  // }
 }
