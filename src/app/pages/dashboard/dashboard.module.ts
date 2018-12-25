d90f63ed6445680af4c72d4547255743d7f4e0be
XiaoDi Ma <i@mxd.moe>
2018-12-25 18:17:48 +0800

    add ad

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -4,18 +4,25 @@ import {ThemeModule} from '../../@theme/theme.module';
 import {DashboardComponent} from './dashboard.component';
 import {FinanceHistoryComponent} from './finance-history/finance-history.component';
 import {ExternalLinksComponent} from './external-links/external-links.component';
+import {AdComponent} from './ad/ad.component';
 
 import {DashboardService} from './dashboard.service';
+import {AdsenseModule} from 'ng2-adsense';
 
 const components = [
   DashboardComponent,
   FinanceHistoryComponent,
   ExternalLinksComponent,
+  AdComponent,
 ];
 
 @NgModule({
   imports: [
     ThemeModule,
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-9476186504912637',
+      adSlot: 1855875521,
+    }),
   ],
   exports: [
     ...components,
